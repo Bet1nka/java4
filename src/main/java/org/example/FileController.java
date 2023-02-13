4f16eea527eca610038d0e2b166a07cb9a1aa89d
Bet1nka <betina96@mail.ru>
2023-02-13 16:05:29 +0200

    done

--- src/main/java/org/example/FileController.java
+++ src/main/java/org/example/FileController.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class FileController {
     public  void SAVER(User user) {
 
-        String path = "C:\\Users\\Gambino\\Desktop\\work\\exwork\\src\\main\\java\\contact\\"
+        String path = "C:\\Users\\Asus ROG Strix Scar\\IdeaProjects\\java4\\src\\main\\java\\org\\example\\humans"
                 + user.getFIO()[0] + ".txt";
         try (FileWriter fileWriter = new FileWriter(path, true)){
             System.out.println("контакт успешно запсиан");
